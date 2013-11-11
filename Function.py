102f38ab5860d5793879ac437ac687a10b0e916d
Rita <giordanorita@gmail.com>
2013-11-11 08:55:53 +0100

    Comments

--- Function.py
+++ Function.py
@@ -13,14 +13,15 @@ __status__ = "Beta"
 def matCal(mat):
     s = mat.sum()
     sup = mat.max()
-    sigma = mat.std()
-    return s, sup, sigma
+    return s, sup
 
-
+# Coefficient of Variation
 def cv(f):
+    #cv = np.std(f)/np.mean(f)
     cv = f.std()/f.mean()
     return cv
 
+#Calculation of rms:
 def rmsd(f,n):
     rmsd = np.sqrt(np.sum(np.square(f))/n)
     return rmsd
@@ -28,6 +29,21 @@ def rmsd(f,n):
 def rmsd_r(f1,f2,n):
     rmsd_r = np.sqrt(np.sum(np.square(f1/f2))/n)
     return rmsd_r
+
+
+# Function to calculate the distance in pixel of the spot from the beam center.
+def distPx(Xspot,beamX,Yspot,beamY):
+    distPx = np.sqrt(np.square(Xspot-Xbeam)+np.square(Yspot-Ybeam))
+
+# Distance on the detector from the direct beam.
+def distM(distPx):
+    distM = 0.172 * distPx
+
+#Function defining the resolution of each spot
+# w= wavelength
+def res(w,distM,detectorDistance):
+    res = w/(2*sin(0.5*atan(distM/detectorDistance)))
+
 
 
 # Diplay PILATUS images with the selected spot using red circle.
